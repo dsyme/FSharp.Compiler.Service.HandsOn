85b0b7da0eb78d650ef36c4bbc139a94271247a5
Don <Don@mac.mshome.net>
2014-10-06 04:18:43 -0400

    updates

--- ProjectParser.fsx
+++ ProjectParser.fsx
@@ -10,59 +10,35 @@
 #r "Microsoft.Build.Utilities.v4.0"
 #endif
 
-// Disable warnings for obsolete MSBuild.
-// Mono doesn't support the latest API.
-#nowarn "0044"
 
 open System
 open System.IO
-open Microsoft.Build.BuildEngine
-open Microsoft.Build.Framework
-open Microsoft.Build.Tasks
-open Microsoft.Build.Utilities
 
 type ProjectResolver(uri: string) = 
-    let runningOnMono = Type.GetType("Mono.Runtime") <> null
-    let project = 
-        //if runningOnMono then 
-            new Project(Engine.GlobalEngine,"4.0")
-        //else 
-          //  new Project()
+  
+    let options = 
+
+//      use engine = new Microsoft.Build.Evaluation.ProjectCollection()
+      let project = 
+
+            let xmlReader = System.Xml.XmlReader.Create (uri)
+            let project = Microsoft.Build.Evaluation.ProjectCollection.GlobalProjectCollection .LoadProject(xmlReader,"4.0")
+            project.FullPath <- uri
+            //project.SetProperty ("Configuration", "Debug");
+            //project.SetProperty ("Platform", "");
+
+            Environment.CurrentDirectory <- Path.GetDirectoryName (uri);
+            project //let pi = project.CreateProjectInstance()
+
+            //let b = pi.Build ([| "Build" |], [| |])
+            //pi
  
-    do project.Load(uri)
-   // do printfn "tv: %A" project.ToolsVersion
-    let loadtime = DateTime.Now
-    let mkAbsolute dir v = if Path.IsPathRooted v then v else Path.Combine(dir,v) 
+      let loadtime = DateTime.Now
+      let mkAbsolute dir v = if Path.IsPathRooted v then v else Path.Combine(dir,v) 
+      let fileItems  = project.GetItems("Compile")
+      let dir = Path.GetDirectoryName project.FullPath
 
-    member __.FileName = project.FullFileName
-
-    member __.LoadTime = loadtime
-
-    member __.Directory =  
-      Path.GetDirectoryName project.FullFileName
-
-    member p.Files =
-      let fs  = project.GetEvaluatedItemsByName("Compile")
-      let dir = p.Directory
-      [| for f in fs do yield mkAbsolute p.Directory f.FinalItemSpec |]
-
-    member p.FrameworkVersion = project.GetEvaluatedProperty("TargetFrameworkVersion") 
-
-    // We really want the output of ResolveAssemblyReferences. However, this
-    // needs as input ChildProjectReferences, which is populated by
-    // ResolveProjectReferences. For some reason ResolveAssemblyReferences
-    // does not depend on ResolveProjectReferences, so if we don't run it first
-    // then we won't get the dll files for imported projects in this list.
-    // We can therefore build ResolveReferences, which depends on both of them,
-    // or [|"ResolveProjectReferences";"ResolveAssemblyReferences"|]. These seem
-    // to be equivalent. See Microsoft.Common.targets if you want more info.
-    member p.References =
-      let b = project.Build([|"ResolveReferences"|])
-      [| for i in project.GetEvaluatedItemsByName("ReferencePath") do
-           yield "-r:" + mkAbsolute p.Directory i.FinalItemSpec |]
-
-    member p.Options =
-      let getprop s = project.GetEvaluatedProperty s
+      let getprop s = project.GetPropertyValue s
       let split (s: string) (cs: char[]) =
           if String.IsNullOrWhiteSpace s then [||]
           else s.Split(cs, StringSplitOptions.RemoveEmptyEntries)
@@ -88,10 +64,18 @@ type ProjectResolver(uri: string) =
           if String.IsNullOrWhiteSpace outpath then None
           else 
              let v = Path.Combine(outpath,aname) + (if isLib then ".dll" else ".exe")
-             Some (mkAbsolute p.Directory v)
+             Some (mkAbsolute dir v)
 
-      let docfile = if String.IsNullOrWhiteSpace docfile then None else Some (mkAbsolute p.Directory docfile)
-      let opts = 
+      let docfile = if String.IsNullOrWhiteSpace docfile then None else Some (mkAbsolute dir docfile)
+      let b = project.Build( [| "ResolveReferences" |], null )
+      let files = [| for f in fileItems do yield mkAbsolute dir f.EvaluatedInclude |]
+      let fxVer = project.GetPropertyValue("TargetFrameworkVersion") 
+
+      let references = 
+         [| for i in project.GetItems("ReferencePath") do
+               yield "-r:" + mkAbsolute dir i.EvaluatedInclude |]
+
+      let options = 
        [|
         yield "--simpleresolution"
         yield "--noframework"
@@ -115,29 +99,32 @@ type ProjectResolver(uri: string) =
         yield if optimize then "--optimize+" else "--optimize-"
         yield if tailcalls then "--tailcalls+" else "--tailcalls-"
         yield! otherflags
-        yield! p.References
-        yield! p.Files
-       |]
+        yield! references
+        yield! files
+        |]
+      options
 
-    // Workarounds for various reference resolution glitches on different installations, reassons TBD
-(*
-      let opts = 
-           [| for opt in opts do 
-                 yield opt.Replace("mono/2.0","mono/4.0")
-              if not (opts |> Array.exists (fun opt -> opt.Contains("FSharp.Core"))) then 
-                if (try File.Exists "/Library/Frameworks/Mono.framework/Versions/3.8.0/lib/mono/4.0/FSharp.Core.dll" with _ -> false) then
-                    yield "/Library/Frameworks/Mono.framework/Versions/3.8.0/lib/mono/4.0/FSharp.Core.dll"
-                if (try File.Exists @"C:\Program Files (x86)\Reference Assemblies\Microsoft\FSharp\3.0\Runtime\v4.0\FSharp.Core.dll" with _ -> false) then
-                    yield @"C:\Program Files (x86)\Reference Assemblies\Microsoft\FSharp\3.0\Runtime\v4.0\FSharp.Core.dll"
-            |]
-*)
-      opts
+    member p.Options = options
 
-(*
-let fsproj = __SOURCE_DIRECTORY__ + @"/example/example.fsproj"
 
-let v = ProjectResolver(fsproj) 
+[<STAThread; EntryPoint>]
+let main(args) = 
 
-for x in v.Options do printfn "%s" x
-*)
+    let v = ProjectResolver(args.[0]) 
 
+    for x in v.Options do printfn "%s" x
+
+    //let b = Microsoft.Build.Execution.BuildManager()
+    //let nm = typeof<Microsoft.Build.Execution.BuildManager>.InvokeMember("BuildNodeManager", System.Reflection.BindingFlags.NonPublic  ||| System.Reflection.BindingFlags.GetProperty ||| System.Reflection.BindingFlags.Instance, null, b, [| |])
+
+//    let nm = b
+    //b.Finalize()
+    //printfn "done"
+
+    System.GC.Collect()
+    try 
+          System.GC.WaitForPendingFinalizers()
+    with _ -> () 
+
+
+    0
